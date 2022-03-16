e82e4c3ca15275e6c00d23635f7878e2864df8fb
Mayur Rumalwala <mrumalwala@streamlinksoftware.com>
2022-03-15 23:38:38 -0400

    Add project files.

---
+++ PulseImageProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PulseImageProject.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
